6f1a5316f29638992db55374989d9999d1b2a3fc
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-28 12:54:15 +0100

    feature: script of for creating aws stack with cloud formation

--- pyspark/scripts/deploy-stack.sh
+++ pyspark/scripts/deploy-stack.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e # failed fast. Exit script as soon as error occured. Prevent end less run  
 
-# Make sure to replace the ParameterValue for GitHubRepo below
-aws cloudformation create-stack \ 
-    --region us-east-1 \ 
-    --stack-name ldi_sparkdemo \ 
-    --template-body file://./cloud-formation-stack-template.yaml \ 
-    --capabilities CAPABILITY_NAMED_IAM \ 
-    --parameters ParameterKey=GitHubRepo,ParameterValue=USERNAME/REPO ParameterKey=CreateOIDCProvider,ParameterValue=true
+#STACK_NAME=$1
+STACK_NAME="ldisparkserverless"
+REPO="xvanausloos/ci-cd-serverless-spark"
+TEMPLATE_FILE="cloud-formation-stack-template.yaml"
+
+if [ -z "$1" ]
+  then
+    echo "No STACK_NAME argument supplied"
+    exit 1
+fi
+
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://$TEMPLATE_FILE \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=GitHubRepo,ParameterValue=${REPO} ParameterKey=CreateOIDCProvider,ParameterValue=true
+    
